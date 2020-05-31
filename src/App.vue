1a6b1e7de3e90265378d605631b8a8b6d1b6f59c
leandrocodes <leandrovianacodes@gmail.com>
2020-05-30 23:03:35 -0400

    vue apollo settings

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,56 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <input type="text" v-model="email" placeholder="email" />
+    <input type="text" v-model="password" placeholder="senha" />
+    <button @click="login">Login</button>
+    <div v-if="loading"><p>carregando...</p></div>
+    <div v-if="user">{{ user }}</div>
     <router-view />
   </div>
 </template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  data: () => ({
+    email: '',
+    password: '',
+    user: null,
+    loading: false
+  }),
+  methods: {
+    async login() {
+      this.loading = true
+      try {
+        this.user = await this.$apollo.mutate({
+          mutation: gql`
+            mutation($email: String!, $password: String!) {
+              login(email: $email, password: $password) {
+                email
+                username
+                points
+                sweeties
+                bonuses
+              }
+            }
+          `,
+          variables: {
+            email: this.email,
+            password: this.password
+          }
+        })
+
+        if (this.user) this.loading = false
+      } catch (err) {
+        this.loading = false
+        console.log(err.graphQLErrors)
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
